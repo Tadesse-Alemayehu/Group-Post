fa3d4f5cdd8e34740da61a4f1cfa2da0ae68ccaa
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-12-27 11:00:42 +0300

    Update toutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
+  resources :users, only: %i[index] do
   resources :groups
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
