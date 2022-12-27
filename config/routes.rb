7d222ba11733d93bfbc6c8532a8565e8bead2eab
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-12-27 10:56:59 +0300

    Configre devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  devise_for :users
+  root 'users#index'
   resources :groups
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
