739785cfd816bc651a7210cdb4b732f8a8a3f46f
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-12-27 15:43:24 +0300

    Update group model with methods to count posts and groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,11 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :user_groups
+  has_many :posts
+  def members_count
+    self.user_groups.count
+  end
+  def posts_count
+    self.posts.count
+  end
 end
