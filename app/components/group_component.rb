866fd422417e6046ac70f3594647b749b4fc7ba4
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-12-27 18:09:51 +0300

    Update: edit based on can validation

--- app/components/group_component.rb
+++ app/components/group_component.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class GroupComponent < ViewComponent::Base
+include Devise::Controllers::Helpers
   def initialize(group:, current_user:)
     @group = group
     @current_user=current_user
